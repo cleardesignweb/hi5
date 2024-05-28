81580981f7cc09f93a55ee5830a45545da665980
cleardesignweb <149852552+cleardesignweb@users.noreply.github.com>
2024-05-27 22:25:03 -0500

    testig pull

---
+++ testfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const testfile = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default testfile
+
+const styles = StyleSheet.create({})
